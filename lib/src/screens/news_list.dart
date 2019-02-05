53a31f2efe93636dd9d21aaa40d9b86170fd72f8
plokmij <samfan@live.com>
2019-02-06 00:31:05 +0530

    Add sample on demand widget rendering

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class NewsList extends StatelessWidget{
   Widget build(context) {
@@ -6,7 +7,31 @@ class NewsList extends StatelessWidget{
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show some news here'),
+      body: buildList(),
     );
   }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? Text('I m visible $index')
+                : Text('I havent fetched data yet $index');
+          }
+        );
+      }
+    );
+  }
+ 
+  getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'hi',
+    );
+  }
+
 }
